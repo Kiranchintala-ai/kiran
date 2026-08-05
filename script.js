9bfa4ad4612a537c62bda6b79fc2c7774171d315
Kiranchintala-ai <chintalakiran0@gmail.com>
2026-08-05 19:40:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const API_KEY = "PASTE_YOUR_OPENROUTER_API_KEY_HERE";
-
 document.getElementById("imageUpload").addEventListener("change", function () {
     const file = this.files[0];
+
     if (!file) return;
 
     const preview = document.getElementById("preview");
@@ -9,23 +8,43 @@ document.getElementById("imageUpload").addEventListener("change", function () {
     preview.style.display = "block";
 });
 
-async function analyzeChart() {
-    const result = document.getElementById("result");
-    result.innerHTML = "🤖 AI is analyzing...";
+function analyzeChart() {
 
     const file = document.getElementById("imageUpload").files[0];
 
     if (!file) {
-        result.innerHTML = "Please select an image first.";
+        document.getElementById("result").innerHTML =
+            "<h3>Please select a chart image.</h3>";
         return;
     }
 
-    result.innerHTML =
-`✅ Frontend is working!
+    document.getElementById("result").innerHTML =
+        "<h3>📊 Analysis Completed</h3>";
 
-Image Selected:
-${file.name}
+    document.getElementById("signal").innerHTML =
+        "<h2 style='color:green;'>🟢 BUY</h2>";
 
-Next Step:
-Now we will connect the AI API.`;
+    document.getElementById("entry").innerHTML =
+        "<b>Entry Price:</b> 24500";
+
+    document.getElementById("stoploss").innerHTML =
+        "<b>Stop Loss:</b> 24380";
+
+    document.getElementById("target").innerHTML =
+        "<b>Target Price:</b> 24750";
+        document.getElementById("trendCell").innerHTML = "Uptrend";
+document.getElementById("signalCell").innerHTML = "BUY";
+document.getElementById("confidenceCell").innerHTML = "82%";
+}
+
+function clearResult() {
+
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("signal").innerHTML = "";
+    document.getElementById("entry").innerHTML = "";
+    document.getElementById("stoploss").innerHTML = "";
+    document.getElementById("target").innerHTML = "";
+
+    document.getElementById("preview").style.display = "none";
+    document.getElementById("imageUpload").value = "";
 }
